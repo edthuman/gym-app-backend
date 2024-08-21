4c56a2884b99c62e0eb5fb2933e80ad6061b6cd8
edthuman <edwardadirvine@aol.com>
2024-08-21 21:51:51 +0100

    add passing test using promises

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -4,14 +4,14 @@ import request from 'supertest'
 const endpoints = require("../endpoints.json")
 
 describe("GET /api", ()=>{
-    it("returns the expected json file", async () => {
-        const response = await request(app)
+    it("returns the expected json file", () => {
+        return request(app)
         .get("/api")
-
-        const parsedResponse = JSON.parse(response.text)
-
-        expect(response.statusCode).toBe(200)
-        expect(parsedResponse).toEqual(endpoints)
+        .expect(200)
+        .then((response) => {
+            const parsedResponse = JSON.parse(response.text)
+            expect(parsedResponse).toEqual(endpoints)
+        })
     })
 
 })
