25ecd963644b09ff9221fd162d6b05fcff487c67
edthuman <edwardadirvine@aol.com>
2024-08-21 21:48:22 +0100

    add passing test for /api endpoint

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,21 +1,17 @@
-import { readFile } from "fs";
 import app from "../src";
-
 import request from 'supertest'
 
+const endpoints = require("../endpoints.json")
+
 describe("GET /api", ()=>{
-    it("returns the expected json file", async() => {
+    it("returns the expected json file", async () => {
         const response = await request(app)
         .get("/api")
 
-        readFile(`${__dirname}/../endpoints.json`, { encoding: "utf-8" }, (err, expectedEndpoints) => {
-            if (err) {
-                console.log("Error obtaining endpoints.json \n", err)
-            }
-            expect(response.statusCode).toBe(200)
-            expect(response.text).toBe(expectedEndpoints)
-        })
+        const parsedResponse = JSON.parse(response.text)
 
-        //test is passing before readfile completes
+        expect(response.statusCode).toBe(200)
+        expect(parsedResponse).toEqual(endpoints)
     })
+
 })
