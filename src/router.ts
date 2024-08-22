eb0b3176efbe3e1dd7afbc080702fb5cb9ca45ad
edthuman <edwardadirvine@aol.com>
2024-08-22 12:48:01 +0100

    add POST /api functionality

--- src/router.ts
+++ src/router.ts
@@ -3,15 +3,19 @@ import { readFile } from "fs";
 
 const router = express.Router()
 
-router.get('/api', (req: Request, res: Response) => {
-    readFile(`${__dirname}/../endpoints.json`, { encoding: "utf-8" }, (err, endpointsJSON)=>{
-      if (err) {
-        res.status(500).send(err)
-      } else {
-        const parsedEndpoints = JSON.parse(endpointsJSON)
-        res.send({ endpoints : parsedEndpoints })
-      }
-    })
+router.get("/api", (req: Request, res: Response) => {
+  readFile(`${__dirname}/../endpoints.json`, { encoding: "utf-8" }, (err, endpointsJSON)=>{
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      const parsedEndpoints = JSON.parse(endpointsJSON)
+      res.send({ endpoints : parsedEndpoints })
+    }
+  })
 });
+
+router.post("/api", (req: Request, res: Response) => {
+  res.status(405).send({ msg : "Request method not allowed on this endpoint"})
+})
 
 export default router
