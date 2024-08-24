cfb53bbce43ffcb65ce2cb78695c07d7a0ccccc7
edthuman <edwardadirvine@aol.com>
2024-08-24 18:32:11 +0100

    refactor sendMethodNotAllowedError to take request parameter

--- src/error-handlers.ts
+++ src/error-handlers.ts
@@ -1,10 +1,10 @@
-import { Response } from "express"
+import { Request, Response } from "express"
 
 const sendNotFoundError = (res: Response) => {
     res.status(404).send({msg: "Requested endpoint does not exist"})
 }
 
-const sendMethodNotAllowedError = (res: Response) => {
+const sendMethodNotAllowedError = (req: Request, res: Response) => {
     res.status(405).send({msg: "Request method not allowed on this endpoint"})
 }
 
