ce0efd0ce939ed3d77e6c8ef8afdb06b251dda60
edthuman <edwardadirvine@aol.com>
2024-08-24 16:09:35 +0100

    add usersRouter to api-router

--- src/api-router.ts
+++ src/api-router.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express"
 import { getEndpoints } from "./controllers/api.controllers";
 import { sendMethodNotAllowedError } from "./error-handlers"
+import usersRouter from "./routers/users-router";
 
 const apiRouter = express.Router()
 
@@ -9,5 +10,7 @@ apiRouter.get("/", getEndpoints);
 apiRouter.all("/", (req: Request, res: Response) => {
   sendMethodNotAllowedError(res)
 })
+
+apiRouter.use("/users", usersRouter)
 
 export default apiRouter
