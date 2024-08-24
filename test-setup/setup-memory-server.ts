7d20640125cbb3f648ef2bc10f170234f65f7aa2
edthuman <edwardadirvine@aol.com>
2024-08-24 12:31:11 +0100

    add afterAll hook to end connection to client

--- test-setup/setup-memory-server.ts
+++ test-setup/setup-memory-server.ts
@@ -1,7 +1,14 @@
+import { MongoClient } from "mongodb";
 import { db } from "../connection";
 import seed from "../src/seed/seed";
 const users = require("../src/seed/data/users.json")
 
 beforeAll(async () => {
     await seed(await db, users);
+})
+
+afterAll(async () => {
+    const database: any = (await db)
+    const client: MongoClient = database.client
+    client.close()
 })
