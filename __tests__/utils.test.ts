b4bf38a9c32156cb5e5154da9c8c912386b0ac5f
edthuman <edwardadirvine@aol.com>
2024-08-24 18:08:50 +0100

    add completed tests for generateUserErrorMessage function - renamed from validateUserInput

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,11 +1,53 @@
-import { validateUserInput } from "../src/utils/user.utils";
+import { generateUserErrorMessage } from "../src/utils/user.utils";
 
-describe("validateUserInput", () => {
+describe("generateUserErrorMessage", () => {
     it("returns an empty string if provided a valid user object", () => {
         const user = { username: "Jeff"}
         
-        const output = validateUserInput(user)
+        const output = generateUserErrorMessage(user)
 
         expect(output).toBe("")
     })
+    it("returns correct error message if user object is empty", () => {
+        const user = {}
+        
+        const output = generateUserErrorMessage(user)
+
+        expect(output).toBe("No request body given")
+    })
+    it("returns correct error message if user object has no username", () => {
+        const user = { key: "value"}
+        
+        const output = generateUserErrorMessage(user)
+
+        expect(output).toBe("No username given")
+    })
+    it("returns correct error message if username is an empty string", () => {
+        const user = { username: ""}
+        
+        const output = generateUserErrorMessage(user)
+
+        expect(output).toBe("No username given")
+    })
+    it("returns correct error message if username is not a string", () => {
+        const numberUsername = { username: 3}
+        const numberUsernameOutput = generateUserErrorMessage(numberUsername)
+        expect(numberUsernameOutput).toBe("Username must be a string")
+
+        const arrayUsername = { username: ["username"]}
+        const arrayUsernameOutput = generateUserErrorMessage(arrayUsername)
+        expect(arrayUsernameOutput).toBe("Username must be a string")
+
+        const objectUsername = { username: {username: "username"}}
+        const objectUsernameOutput = generateUserErrorMessage(objectUsername)
+        expect(objectUsernameOutput).toBe("Username must be a string")
+    })
+    it("returns correct error message if user object has extra properties", () => {
+        const user = { username: "Jeff", age: 22}
+        
+        const output = generateUserErrorMessage(user)
+
+        expect(output).toBe("Request body should only provide a username")
+    })
+    // no need for a test checking user is an object, as this is req.body
 })
