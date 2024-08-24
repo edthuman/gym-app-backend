01724480c501a4d7a2431551faf3491cb82bdff5
edthuman <edwardadirvine@aol.com>
2024-08-24 13:06:36 +0100

    add / describe block for easier collapsing of /api endpoint tests

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -4,46 +4,48 @@ import request from 'supertest';
 const endpoints = require("../endpoints.json")
 
 describe("/api", ()=>{
-    test("GET 200: returns the expected json file", () => {
-        const expectedEndpoints = { endpoints }
-
-        return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-            expect(body).toEqual(expectedEndpoints)
+    describe("/", ()=>{
+        test("GET 200: returns the expected json file", () => {
+            const expectedEndpoints = { endpoints }
+    
+            return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body }) => {
+                expect(body).toEqual(expectedEndpoints)
+            })
         })
-    })
-    test("POST 405: returns a Method Not Allowed error message", () => {
-        return request(app)
-        .post("/api")
-        .expect(405)
-        .then(({ body: { msg }}) => {
-            expect(msg).toBe("Request method not allowed on this endpoint")
+        test("POST 405: returns a Method Not Allowed error message", () => {
+            return request(app)
+            .post("/api")
+            .expect(405)
+            .then(({ body: { msg }}) => {
+                expect(msg).toBe("Request method not allowed on this endpoint")
+            })
         })
-    })
-    test("PATCH 405: returns a Method Not Allowed error message", () => {
-        return request(app)
-        .patch("/api")
-        .expect(405)
-        .then(({ body : { msg }}) => {
-            expect(msg).toBe("Request method not allowed on this endpoint")
+        test("PATCH 405: returns a Method Not Allowed error message", () => {
+            return request(app)
+            .patch("/api")
+            .expect(405)
+            .then(({ body : { msg }}) => {
+                expect(msg).toBe("Request method not allowed on this endpoint")
+            })
         })
-    })
-    test("DELETE 405: returns a Method Not Allowed error message", () => {
-        return request(app)
-        .delete("/api")
-        .expect(405)
-        .then(({ body : { msg }}) => {
-            expect(msg).toBe("Request method not allowed on this endpoint")
+        test("DELETE 405: returns a Method Not Allowed error message", () => {
+            return request(app)
+            .delete("/api")
+            .expect(405)
+            .then(({ body : { msg }}) => {
+                expect(msg).toBe("Request method not allowed on this endpoint")
+            })
         })
-    })
-    test("PUT 405: returns a Method Not Allowed error message", () => {
-        return request(app)
-        .put("/api")
-        .expect(405)
-        .then(({ body : { msg }}) => {
-            expect(msg).toBe("Request method not allowed on this endpoint")
+        test("PUT 405: returns a Method Not Allowed error message", () => {
+            return request(app)
+            .put("/api")
+            .expect(405)
+            .then(({ body : { msg }}) => {
+                expect(msg).toBe("Request method not allowed on this endpoint")
+            })
         })
     })
     describe("/users", () => {
@@ -56,7 +58,7 @@ describe("/api", ()=>{
 
                 users.forEach((user: any) => {
                     const numberOfProperties = Object.keys(user).length
-                    
+
                     expect(numberOfProperties).toBe(2)
                     
                     expect(user).toMatchObject({
