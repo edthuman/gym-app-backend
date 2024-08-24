597c68f7a7fbd3bbccae60b550b092874fcc4a67
edthuman <edwardadirvine@aol.com>
2024-08-24 16:47:44 +0100

    add postUser controller

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express"
-import { selectAllUsers } from "../models/users.models"
+import { insertUser, selectAllUsers } from "../models/users.models"
 
 const getUsers = async (req: Request, res: Response) => {
     const users = await selectAllUsers()
     res.send({ users })
 }
 
-export { getUsers }
+const postUser = async (req: Request, res: Response) => {
+    const userObject = req.body
+    const user = await insertUser(userObject)
+    res.status(201).send({ user })
+}
+
+export { getUsers, postUser }
