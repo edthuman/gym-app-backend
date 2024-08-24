2a097c123587487372fc61069e4bf5fa21624537
edthuman <edwardadirvine@aol.com>
2024-08-24 12:34:23 +0100

    add stopping of memory server to afterAll hook

--- test-setup/setup-memory-server.ts
+++ test-setup/setup-memory-server.ts
@@ -1,7 +1,9 @@
 import { MongoClient } from "mongodb";
 import { db } from "../connection";
 import seed from "../src/seed/seed";
-const users = require("../src/seed/data/users.json")
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+const users = require("../src/seed/data/users.json");
 
 beforeAll(async () => {
     await seed(await db, users);
@@ -12,4 +14,7 @@ afterAll(async () => {
     const database: any = (await db)
     const client: MongoClient = database.client
     client.close()
+
+    const memoryServer: MongoMemoryServer = (global as any).__MONGOINSTANCE;
+    memoryServer.stop()
 })
