787a30a70dd094d4f710ab0362db2a721c71c837
edthuman <edwardadirvine@aol.com>
2024-08-24 18:27:59 +0100

    add logic for PATCH/DELETE/PUT request to users endpoint

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,10 +1,13 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import { getUsers, postUser } from "../controllers/users.controllers"
+import { sendBadRequestError, sendMethodNotAllowedError } from "../error-handlers"
 
 const usersRouter = express.Router()
 
 usersRouter.get("/", getUsers)
-
 usersRouter.post("/", postUser)
+usersRouter.all("/", (req: Request, res: Response) => {
+    sendMethodNotAllowedError(res)
+})
 
 export default usersRouter
