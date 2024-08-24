f78aa5aaecb84f9a06a98420b168082755340401
edthuman <edwardadirvine@aol.com>
2024-08-24 17:09:43 +0100

    add logic to handle empty body POST request

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { insertUser, selectAllUsers } from "../models/users.models"
+import { sendBadRequestError } from "../error-handlers"
 
 const getUsers = async (req: Request, res: Response) => {
     const users = await selectAllUsers()
@@ -8,8 +9,12 @@ const getUsers = async (req: Request, res: Response) => {
 
 const postUser = async (req: Request, res: Response) => {
     const userObject = req.body
-    const user = await insertUser(userObject)
-    res.status(201).send({ user })
+    if (Object.keys(userObject).length === 0) {
+        sendBadRequestError(res, "No request body given")
+    } else {
+        const user = await insertUser(userObject)
+        res.status(201).send({ user })
+    }
 }
 
 export { getUsers, postUser }
