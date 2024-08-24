4d3190e400602713fc81dd167846be54d5d0fa91
edthuman <edwardadirvine@aol.com>
2024-08-24 16:51:54 +0100

    add postUser controller to usersRouter

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,8 +1,10 @@
 import express from "express"
-import { getUsers } from "../controllers/users.controllers"
+import { getUsers, postUser } from "../controllers/users.controllers"
 
 const usersRouter = express.Router()
 
 usersRouter.get("/", getUsers)
+
+usersRouter.post("/", postUser)
 
 export default usersRouter
