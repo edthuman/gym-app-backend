5d61c10438dc58a88a2f00ae99b3db4db9aca494
edthuman <edwardadirvine@aol.com>
2024-08-25 14:58:52 +0100

    add test for sort query with id given as value

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -202,6 +202,21 @@ describe("/api", ()=>{
                         expect(users).toEqual(orderedUsers)
                     })
                 })
+                test("returns all users ordered by _id when queried with id", () => {
+                    return request(app)
+                    .get("/api/users?sort=id")
+                    .expect(200)
+                    .then(({body: {users}}) => {
+                        const orderedUsers = users.toSorted((a: MongoDBUser, b: MongoDBUser)=>{
+                            const x = a._id.toLowerCase()
+                            const y = b._id.toLowerCase()
+                            if (x < y) return -1
+                            if (x > y) return 1
+                            return 0
+                        })
+                        expect(users).toEqual(orderedUsers)
+                    })
+                })
             })
         })
     })
