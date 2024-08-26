5533270dd56b0bb08f55062db1d1d8f3f6159aaa
edthuman <edwardadirvine@aol.com>
2024-08-26 17:23:56 +0100

    add return statement to getEndpoints error handling

--- src/controllers/api.controllers.ts
+++ src/controllers/api.controllers.ts
@@ -6,6 +6,7 @@ export const getEndpoints = async (req: Request, res: Response) => {
     const endpoints = selectEndpoints()
     if (endpoints === "") {
         sendInternalServerError(res, "Error fetching endpoints")
+        return
     }
     res.send({ endpoints })
 }
