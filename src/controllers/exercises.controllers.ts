33866d8f8ec379aad6e83e66c749008e3e864bf9
edthuman <edwardadirvine@aol.com>
2024-08-26 18:51:19 +0100

    add error handling for failed selectAllUsers to getExercises controller

--- src/controllers/exercises.controllers.ts
+++ src/controllers/exercises.controllers.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 import { selectAllExercises } from "../models/exercises.models";
+import { sendInternalServerError } from "../error-handlers";
 
 export const getExercises = async (req: Request, res: Response) => {
     const exercises = await selectAllExercises()
+    const isError = exercises.length === 0
+    if (isError) {
+        sendInternalServerError(res, "Error fetching exercises")
+    }
     res.status(200).send({ exercises })
 }
