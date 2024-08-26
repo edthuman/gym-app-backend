05874928ba36901e5c7d191fd7e97eb4f750ff20
edthuman <edwardadirvine@aol.com>
2024-08-26 18:31:15 +0100

    update seed function to seed exercises collection

--- src/seeding/seed.ts
+++ src/seeding/seed.ts
@@ -1,9 +1,11 @@
 import { Db } from "mongodb"
-import { UserInput } from "../types";
+import { Exercise, User } from "../types";
 
-const seed = async (db: Db, users: UserInput[]) => {
+const seed = async (db: Db, users: User[], exercises: Exercise[]) => {
     await db.collection("users").drop()
     await db.collection("users").insertMany(users);
+    await db.collection("exercised").drop()
+    await db.collection("exercises").insertMany(exercises)
 }
 
 export default seed
