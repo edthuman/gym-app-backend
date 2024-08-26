bf9555ac28092f959b75d4526e1aa19dfcdd18fb
edthuman <edwardadirvine@aol.com>
2024-08-26 17:22:36 +0100

    add error handling to getEndpoints controller

--- src/controllers/api.controllers.ts
+++ src/controllers/api.controllers.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { selectEndpoints } from "../models/api.models";
+import { sendInternalServerError } from "../error-handlers";
 
 export const getEndpoints = async (req: Request, res: Response) => {
     const endpoints = selectEndpoints()
+    if (endpoints === "") {
+        sendInternalServerError(res, "Error fetching endpoints")
+    }
     res.send({ endpoints })
 }
