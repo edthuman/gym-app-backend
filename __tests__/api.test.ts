893286393b71590d6c01cb9b8355d2d440f02b96
edthuman <edwardadirvine@aol.com>
2024-08-27 17:38:16 +0100

    change toMatchObject statements to toEqual

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -63,7 +63,7 @@ describe("/api", () => {
     
                         expect(numberOfProperties).toBe(2)
                         
-                        expect(user).toMatchObject({
+                        expect(user).toEqual({
                             _id: expect.any(String),
                             username: expect.any(String)
                         })
@@ -78,7 +78,7 @@ describe("/api", () => {
                 .send(userObject)
                 .expect(201)
                 .then(({body: { user }}) => {
-                    expect(user).toMatchObject({
+                    expect(user).toEqual({
                         _id: expect.any(String),
                         username: expect.stringMatching(userObject.username)
                     })
@@ -387,7 +387,7 @@ describe("/api", () => {
                     expect(exercises).toHaveLength(6)
 
                     exercises.forEach((exercise: MongoDBExercise[]) => {
-                        expect(exercise).toMatchObject({
+                        expect(exercise).toEqual({
                             _id: expect.any(String),
                             name: expect.any(String),
                             description: expect.any(String),
