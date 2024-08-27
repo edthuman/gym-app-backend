5f828d3109a2eda126834fe39370890ed3af2253
edthuman <edwardadirvine@aol.com>
2024-08-27 14:34:52 +0100

    add tests for generateExerciseErrorMessage invoked with object missing properties

--- __tests__/exercise-utils.test.ts
+++ __tests__/exercise-utils.test.ts
@@ -1,15 +1,54 @@
 import { generateExerciseErrorMessage } from "../src/utils/exercise.utils";
 
 describe("generateExerciseErrorMessage", () => {
-    test("returns an empty string for a valid exercise object", () => {
-        const exercise = { 
+    it("returns an empty string for a valid exercise object", () => {
+        const input = { 
             name: "exercise name",
             description: "description",
             icon: "filename"
         }
 
-        const output = generateExerciseErrorMessage(exercise)
+        const output = generateExerciseErrorMessage(input)
 
         expect(output).toBe("")
+    })
+    it("returns correct error message when given an empty object", () => {
+        const input = {}
+        const output = generateExerciseErrorMessage(input)
+
+        expect(output).toBe("No request body given")
+    })
+    it("returns correct error message when exercise missing name property", () => {
+        const input = { 
+            description: "description",
+            icon: "filename"
+        }
+        const output = generateExerciseErrorMessage(input)
+
+        expect(output).toBe("No name given")
+    })
+    it("returns correct error message when exercise missing description property", () => {
+        const input = { 
+            name: "exercise name",
+            icon: "filename"
+        }
+        const output = generateExerciseErrorMessage(input)
+
+        expect(output).toBe("No description given")
+    })
+    it("returns correct error message when exercise missing icon property", () => {
+        const input = { 
+            name: "exercise name",
+            description: "description",
+        }
+        const output = generateExerciseErrorMessage(input)
+
+        expect(output).toBe("No icon given")
+    })
+    it("returns correct error message when missing multiple properties", () => {
+        const input = { icon: "file" }
+        const output = generateExerciseErrorMessage(input)
+
+        expect(output).toBe("No name given")
     })
 })
