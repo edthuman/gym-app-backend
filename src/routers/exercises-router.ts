a65e820d3624bd1789b8e76cc9cce5521de61d3b
edthuman <edwardadirvine@aol.com>
2024-08-27 11:18:29 +0100

    add sendMethodNotAllowedError to exercisesRouter

--- src/routers/exercises-router.ts
+++ src/routers/exercises-router.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import { getAllExercises } from "../controllers/exercises.controllers"
+import { sendMethodNotAllowedError } from "../error-handlers"
 
 const exercisesRouter = express.Router()
 
 exercisesRouter.get("/", getAllExercises)
+exercisesRouter.use("/", sendMethodNotAllowedError)
 
 export default exercisesRouter
