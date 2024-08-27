454bd196d0a88a34c981270389b7c02f94feec66
edthuman <edwardadirvine@aol.com>
2024-08-27 11:50:24 +0100

    add postExercise controller

--- src/controllers/exercises.controllers.ts
+++ src/controllers/exercises.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { selectAllExercises } from "../models/exercises.models";
+import { insertExercise, selectAllExercises } from "../models/exercises.models";
 import { sendInternalServerError } from "../error-handlers";
 
 export const getAllExercises = async (req: Request, res: Response) => {
@@ -9,4 +9,10 @@ export const getAllExercises = async (req: Request, res: Response) => {
         sendInternalServerError(res, "Error fetching exercises")
     }
     res.status(200).send({ exercises })
+}
+
+export const postExercise = async (req: Request, res: Response) => {
+    const exerciseInput = req.body
+    const exercise = await insertExercise(exerciseInput)
+    res.status(201).send({exercise})
 }
