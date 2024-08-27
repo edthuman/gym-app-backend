55a63e5d525d1372fe02e144b0d3f07928e5e19f
edthuman <edwardadirvine@aol.com>
2024-08-27 15:28:08 +0100

    add getExerciseErrorMessage handling for non-string properties

--- src/utils/exercise.utils.ts
+++ src/utils/exercise.utils.ts
@@ -1,19 +1,29 @@
 export const getExerciseErrorMessage = (exercise: any) => {
     const numberOfProperties = Object.keys(exercise).length
+    const { name, description, icon } = exercise
     
     if (numberOfProperties === 0) {
         return "No request body given"
     }
 
     // checks for missing properties, returned error message states first missing property found
-    if (!exercise.name) {
+    if (!name) {
         return "No name given"
     }
-    if (!exercise.description){
+    if (typeof name !== "string") {
+        return "Name must be a string"
+    }
+    if (!description) {
         return "No description given"
     }
-    if (!exercise.icon) {
+    if (typeof description !== "string") {
+        return "Description must be a string"
+    }
+    if (!icon) {
         return "No icon given"
+    }
+    if (typeof icon !== "string") {
+        return "Icon must be a string"
     }
 
     return ""
