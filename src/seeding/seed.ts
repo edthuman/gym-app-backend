36d48e74ecd9c2cc3ccc158936ff2d9f31c30f12
edthuman <edwardadirvine@aol.com>
2024-08-29 11:32:33 +0100

    fix typo in name of exercises collection

--- src/seeding/seed.ts
+++ src/seeding/seed.ts
@@ -4,7 +4,7 @@ import { Exercise, User } from "../types";
 const seed = async (db: Db, users: User[], exercises: Exercise[]) => {
     await db.collection("users").drop()
     await db.collection("users").insertMany(users);
-    await db.collection("exercised").drop()
+    await db.collection("exercises").drop()
     await db.collection("exercises").insertMany(exercises)
 }
 
