a8e13ed7dd5e2d65371abc2ad374147ba1981fad
edthuman <edwardadirvine@aol.com>
2024-08-30 14:00:09 +0100

    add query handling to getEndpoints

--- src/controllers/api.controllers.ts
+++ src/controllers/api.controllers.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { selectEndpoints } from "../models/api.models";
-import { sendInternalServerError } from "../error-handlers";
+import { sendBadRequestError, sendInternalServerError } from "../error-handlers";
 
 export const getEndpoints = async (req: Request, res: Response) => {
+    const isQuery = Object.keys(req.query).length !== 0
+
+    if (isQuery) {
+        sendBadRequestError(res, "Invalid query")
+        return
+    }
+
     const endpoints = selectEndpoints()
     if (endpoints === "") {
         sendInternalServerError(res, "Error fetching endpoints")
