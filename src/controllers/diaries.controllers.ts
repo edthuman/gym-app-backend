5dc549a19742da818aac49326ac0510f315cf07c
edthuman <edwardadirvine@aol.com>
2024-09-01 14:17:14 +0100

    add handling for selectAllDiaries error to getAllDiaries

--- src/controllers/diaries.controllers.ts
+++ src/controllers/diaries.controllers.ts
@@ -1,7 +1,12 @@
 import { Response, Request } from "express"
 import { selectAllDiaries } from "../models/diaries.models"
+import { sendInternalServerError } from "../error-handlers"
 
 export const getAllDiaries = async (req: Request, res: Response) => {
-    const diaries = await selectAllDiaries()
+    const diaries: any = await selectAllDiaries()
+    if (diaries.isError) {
+        sendInternalServerError(res, "Error fetching diaries")
+        return
+    }
     res.send({ diaries })
 }
