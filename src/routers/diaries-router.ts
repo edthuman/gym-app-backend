be28c72161d3fc71447a90f7df7c98f80ace7c40
edthuman <edwardadirvine@aol.com>
2024-09-01 14:43:04 +0100

    add postDiary to diariesRouter

--- src/routers/diaries-router.ts
+++ src/routers/diaries-router.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { Request, Response } from "express";
-import { getAllDiaries } from "../controllers/diaries.controllers";
+import { getAllDiaries, postDiary } from "../controllers/diaries.controllers";
 
 const diariesRouter = express.Router()
 
 const diaries = require("../seeding/data/diaries.json")
 
 diariesRouter.get("/", getAllDiaries)
+
+diariesRouter.post("/", postDiary)
 
 export default diariesRouter
