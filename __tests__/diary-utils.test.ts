5999362b7e0b92e4492208cc9a2af05b6d3787c4
edthuman <edwardadirvine@aol.com>
2024-09-01 16:48:11 +0100

    add tests for generateDiaryErrorMessage getting a non-string exercise

--- __tests__/diary-utils.test.ts
+++ __tests__/diary-utils.test.ts
@@ -39,7 +39,7 @@ describe("generateDiaryErrorMessage", () => {
 
         expect(output).toBe("No username given")
     })
-    it("returns correct error string for a diary with a number username", () => {
+    it("returns correct error string for a diary with a number for username", () => {
         const input = {
             username: 1,
             exercise: "Leg Press",
@@ -52,7 +52,7 @@ describe("generateDiaryErrorMessage", () => {
 
         expect(output).toBe("Username must be a string")
     })
-    it("returns correct error string for a diary with an array username", () => {
+    it("returns correct error string for a diary with an array for username", () => {
         const input = {
             username: ["gymbro"],
             exercise: "Leg Press",
@@ -65,7 +65,7 @@ describe("generateDiaryErrorMessage", () => {
 
         expect(output).toBe("Username must be a string")
     })
-    it("returns correct error string for a diary with an object username", () => {
+    it("returns correct error string for a diary with an object for username", () => {
         const input = {
             username: { name: "gymbro" },
             exercise: "Leg Press",
@@ -102,5 +102,44 @@ describe("generateDiaryErrorMessage", () => {
         const output = generateDiaryErrorMessage(input)
 
         expect(output).toBe("No exercise given")
+    })
+    it("returns correct error string for a diary with a number for exercise", () => {
+        const input = {
+            username: "gymbro",
+            exercise: 1,
+            personalBest: 2,
+            goal: 4,
+            logs: []
+        }
+
+        const output = generateDiaryErrorMessage(input)
+
+        expect(output).toBe("Exercise must be a string")
+    })
+    it("returns correct error string for a diary with an array for exercise", () => {
+        const input = {
+            username: "gymbro",
+            exercise: ["Leg Press"],
+            personalBest: 2,
+            goal: 4,
+            logs: []
+        }
+
+        const output = generateDiaryErrorMessage(input)
+
+        expect(output).toBe("Exercise must be a string")
+    })
+    it("returns correct error string for a diary with an object for exercise", () => {
+        const input = {
+            username: "gymbro",
+            exercise: { name: "Leg Press" },
+            personalBest: 2,
+            goal: 4,
+            logs: []
+        }
+
+        const output = generateDiaryErrorMessage(input)
+
+        expect(output).toBe("Exercise must be a string")
     })
 })
