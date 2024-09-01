ade6ad9f31236d4449c92573c44f4a48f91e5ae0
edthuman <edwardadirvine@aol.com>
2024-09-01 14:16:47 +0100

    add try catch blocks to selectAllDiaries

--- src/models/diaries.models.ts
+++ src/models/diaries.models.ts
@@ -1,12 +1,17 @@
 import db from "../../connection"
 
 export const selectAllDiaries = async () => {
-    const diariesCluster = await (await db).collection("diaries").find({})
-    const diaries = []
-
-    for await (const diary of diariesCluster) {
-        diaries.push(diary)
+    try {
+        const diariesCluster = await (await db).collection("diaries").find({})
+        const diaries = []
+    
+        for await (const diary of diariesCluster) {
+            diaries.push(diary)
+        }
+    
+        return diaries
     }
-
-    return diaries
+    catch {
+        return {isError: true}
+    }
 }
