d8940ce05d26866d3a9a160e3cc69b5706304378
edthuman <edwardadirvine@aol.com>
2024-09-02 12:10:32 +0100

    remove unused imports and variables from diaries-router.ts

--- src/routers/diaries-router.ts
+++ src/routers/diaries-router.ts
@@ -1,11 +1,8 @@
 import express from "express";
-import { Request, Response } from "express";
 import { getAllDiaries, postDiary } from "../controllers/diaries.controllers";
 import { sendMethodNotAllowedError } from "../error-handlers";
 
 const diariesRouter = express.Router()
-
-const diaries = require("../seeding/data/diaries.json")
 
 diariesRouter.get("/", getAllDiaries)
 diariesRouter.post("/", postDiary)
