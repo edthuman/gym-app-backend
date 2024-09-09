28808c21f242ea61c17ae86b75ce330f4b9a3133
edthuman <edwardadirvine@aol.com>
2024-09-09 14:47:21 +0100

    add deleteDiaryById to diariesRouter

--- src/routers/diaries-router.ts
+++ src/routers/diaries-router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllDiaries, getDiaryById, postDiary } from "../controllers/diaries.controllers";
+import { deleteDiaryById, getAllDiaries, getDiaryById, postDiary } from "../controllers/diaries.controllers";
 import { sendMethodNotAllowedError } from "../error-handlers";
 
 const diariesRouter = express.Router()
@@ -7,6 +7,7 @@ const diariesRouter = express.Router()
 diariesRouter.get("/", getAllDiaries)
 diariesRouter.post("/", postDiary)
 diariesRouter.get("/:diary_id", getDiaryById)
+diariesRouter.delete("/:diary_id", deleteDiaryById)
 diariesRouter.use("/", sendMethodNotAllowedError)
 
 export default diariesRouter
