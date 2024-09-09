5bb395bed556706f22f14115264269b60b437612
edthuman <edwardadirvine@aol.com>
2024-09-09 16:09:20 +0100

    add patchDiary to diaryRouter

--- src/routers/diaries-router.ts
+++ src/routers/diaries-router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteDiary, getAllDiaries, getDiaryById, postDiary } from "../controllers/diaries.controllers";
+import { deleteDiary, getAllDiaries, getDiaryById, patchDiary, postDiary } from "../controllers/diaries.controllers";
 import { sendMethodNotAllowedError } from "../error-handlers";
 
 const diariesRouter = express.Router()
@@ -8,6 +8,7 @@ diariesRouter.get("/", getAllDiaries)
 diariesRouter.post("/", postDiary)
 diariesRouter.get("/:diary_id", getDiaryById)
 diariesRouter.delete("/:diary_id", deleteDiary)
+diariesRouter.patch("/:diary_id", patchDiary)
 diariesRouter.use("/", sendMethodNotAllowedError)
 
 export default diariesRouter
