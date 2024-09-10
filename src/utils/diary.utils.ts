de4478084c2a632c1223e0944f744d6c5e82b0a4
edthuman <edwardadirvine@aol.com>
2024-09-10 12:23:32 +0100

    update spacing in formatPatchObject

--- src/utils/diary.utils.ts
+++ src/utils/diary.utils.ts
@@ -67,7 +67,9 @@ export const checkDiaryQueries = (queries: any[]) => {
 
 export const formatPatchObject = (patchObject: any) => { 
     if (patchObject.hasOwnProperty("logs")) {
-        return { $addToSet: { logs: { $each : [...patchObject.logs] }}}
+        return {
+            $addToSet: { logs: { $each: [...patchObject.logs] }}
+        }
     }   
     return { $set : patchObject }
 } 
