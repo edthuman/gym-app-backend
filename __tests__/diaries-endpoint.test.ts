c8d690214cc3fea6d5ff3d525071e1cced56c964
edthuman <edwardadirvine@aol.com>
2024-09-10 18:45:26 +0100

    add test for personalBest sent to PATCH /diaries/:diary_id being below a log in existing diary

--- __tests__/diaries-endpoint.test.ts
+++ __tests__/diaries-endpoint.test.ts
@@ -1687,5 +1687,18 @@ describe("/api/diaries", () => {
                 expect(msg).toBe("Request should not provide an exercise")
             })
         })
+        test("PATCH 400: returns a Bad Request error message when passed a personalBest below existing logs", async () => {
+            const liftqueenRowingDiary = await (await db).collection("diaries").findOne({ username: "liftqueen", exercise: "Rowing Machine"}) || { _id: "" }
+
+            const id = liftqueenRowingDiary._id.toString()
+            const patchObject = { personalBest: 0 }
+            return request(app)
+            .patch(`/api/diaries/${id}`)
+            .send(patchObject)
+            .expect(400)
+            .then(({body: {msg}}) => {
+                expect(msg).toBe("PersonalBest cannot be below a log")
+            })
+        })
     })
 })
