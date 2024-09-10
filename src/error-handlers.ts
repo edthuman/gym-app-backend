dc7b3a02f20ce61c8292dc588c5422232a833569
edthuman <edwardadirvine@aol.com>
2024-09-10 15:43:20 +0100

    order error-handlers.ts function to status order

--- src/error-handlers.ts
+++ src/error-handlers.ts
@@ -1,13 +1,5 @@
 import { Request, Response } from "express"
 
-export const sendEndpointNotFoundError = (res: Response) => {
-    res.status(404).send({msg: "Requested endpoint does not exist"})
-}
-
-export const sendMethodNotAllowedError = (req: Request, res: Response) => {
-    res.status(405).send({msg: "Request method not allowed on this endpoint"})
-}
-
 export const sendBadRequestError = (res: Response, msg: string) => {
     res.status(400).send({ msg })
 }
@@ -24,14 +16,22 @@ export const sendInvalidOrderError = (res: Response) => {
     res.status(400).send({msg: "Invalid order query"})
 }
 
+export const sendEndpointNotFoundError = (res: Response) => {
+    res.status(404).send({msg: "Requested endpoint does not exist"})
+}
+
+export const sendNotFoundError = (res: Response, msg: string) => {
+    res.status(404).send({msg})
+}
+
+export const sendMethodNotAllowedError = (req: Request, res: Response) => {
+    res.status(405).send({msg: "Request method not allowed on this endpoint"})
+}
+
 export const sendConflictError = (res: Response, msg: string) => {
     res.status(409).send({msg})
 }
 
 export const sendInternalServerError = (res: Response, msg: string) => {
     res.status(500).send({msg})
-}
-
-export const sendNotFoundError = (res: Response, msg: string) => {
-    res.status(404).send({msg})
 }
