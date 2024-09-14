55c21db69f586dd69831dcab9526a72b772bd340
edthuman <edwardadirvine@aol.com>
2024-09-14 18:10:27 +0100

    update connection.ts to allow connection to production database

--- connection.ts
+++ connection.ts
@@ -1,20 +1,28 @@
 const { ServerApiVersion } = require('mongodb');
 import { Db, MongoClient } from "mongodb";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { uri } from "./uri";
+import dotenv from 'dotenv'
 
-const getClient = async () => {
-  let uriToUse
-  if (process.env.NODE_ENV === "test") {
+const ENV = process.env.NODE_ENV || "development"
+const pathToEnvFile = `${__dirname}/.env.${ENV}`;
+
+dotenv.config({
+  path: pathToEnvFile
+});
+
+const getClient = async () => {  
+  if (ENV === "test") {
     // global instance created to allow reuse
     const instance: MongoMemoryServer = await MongoMemoryServer.create();
-    uriToUse = instance.getUri();
+    process.env.URI = instance.getUri();
     (global as any).__MONGOINSTANCE = instance;
-  } else {
-    uriToUse = uri
   }
   
-  const client: MongoClient = new MongoClient(uriToUse, {
+  if (!process.env.URI) {
+    throw new Error("URI not set")
+  }
+
+  const client: MongoClient = new MongoClient(process.env.URI, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
