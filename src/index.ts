c238c0a35801a6201faa287d0168cbfe64a0190b
edthuman <edwardadirvine@aol.com>
2024-09-14 18:45:44 +0100

    add compression to app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express'
 import { sendEndpointNotFoundError } from './error-handlers';
 import apiRouter from './routers/api-router';
+const compression = require("compression");
 
 const app: Express = express();
 
 app.use(express.json())
+
+app.use(compression())
 
 app.use("/api", apiRouter)
 
