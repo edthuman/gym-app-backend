fc590ddb34a304cfaa0b5eb4ff138f6e68a687ef
edthuman <edwardadirvine@aol.com>
2024-09-16 13:16:28 +0100

    add development database seeding data

---
+++ src/seeding/data/development-data/users.ts
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        username: "HumptyDumpty",
+        exercises: [
+            "Leg Press"
+        ]
+    },
+    { 
+        username: "gymbro",
+        exercises: [
+            "Pull Up",
+            "Treadmill"
+        ]
+    },
+    { 
+        username: "liftqueen",
+        exercises: [
+            "Rowing",
+            "Stair Machine"
+        ]
+    },
+    { 
+        username: "legituser",
+        exercises: [
+            "Chin Up"
+        ]
+    },
+    {
+        username: "Steve",
+        exercises: [
+            "Pull Up",
+            "Rowing"
+        ]
+    },
+    {
+        username: "Daisy",
+        exercises: [
+            "Treadmill",
+            "Leg Press"
+        ]
+    }
+]
