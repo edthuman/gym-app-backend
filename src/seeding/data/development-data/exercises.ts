d868701cbe7084040998a5a3976bf961e1746d76
edthuman <edwardadirvine@aol.com>
2024-09-22 19:21:38 +0100

    add category to exercises seeding data

--- src/seeding/data/development-data/exercises.ts
+++ src/seeding/data/development-data/exercises.ts
@@ -2,31 +2,37 @@ module.exports = [
     {
         name: "Pull Up",
         description: "Hang from a bar or handles, pull yourself fully up and then back down",
+        category: "Strength",
         icon: "pull-up"
     },
     {
         name: "Chin Up",
         description: "Hang from a bar or handles, pull yourself up to your chin and then go back down",
+        category: "Strength",
         icon: "chin-up"
     },
     {
         name: "Leg Press",
         description: "From a sitting position, push the weight with both legs out and return",
+        category: "Strength",
         icon: "leg-press"
     },
     {
         name: "Treadmill",
         description: "Walk or run on the machine",
+        category: "Cardio",
         icon: "treadmill"
     },
     {
         name: "Stair Machine",
         description: "Climb stairs continuously",
+        category: "Cardio",
         icon: "stairs"
     },
     {
         name: "Rowing Machine",
         description: "Row back and forth",
+        category: "Cardio",
         icon: "rowing"
     }
 ]
